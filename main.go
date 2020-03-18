c23952cd4b940231835cb11654318629267b121a
Jianhui Li <omegali047@gmail.com>
2020-03-17 23:46:21 -0700

    add router group

--- main.go
+++ main.go
@@ -11,21 +11,31 @@ type Engine struct{}
 func main() {
 	r := omego.New()
 
-	r.GET("/", func(c *omego.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello omego</h1>")
-	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *omego.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello omego</h1>")
+		})
 
-	r.GET("/hello", func(c *omego.Context) {
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
+		v1.GET("/hello", func(c *omego.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *omego.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *omego.Context) {
+			c.JSON(http.StatusOK, omego.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
 
-	r.GET("/hello/:name", func(c *omego.Context) {
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
+	}
 
-	r.GET("/assets/*filepath", func(c *omego.Context) {
-		c.JSON(http.StatusOK, omego.H{"filepath": c.Param("filepath")})
-	})
+	r.Run(":9999")
 
 	r.Run(":9999")
 }
--- omego/omego.go
+++ omego/omego.go
@@ -1,39 +1,34 @@
 package omego
 
 import (
+	"log"
 	"net/http"
 )
 
 // Signature for HandlerFunc
 type HandlerFunc func(*Context)
 
-// Engine implements the interface of ServeHTTP 
+// Engine implements the interface of ServeHTTP
 // router [Key: route] [Value: handler function]
 type Engine struct {
-	router *router
+	*RouterGroup //Embeddding, all the methods of RouterGroup are available on Engine
+	router       *router
+	groups       []*RouterGroup
 }
 
-// Constructor
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc // support middleware
+	parent      *RouterGroup  // support nesting
+	engine      *Engine       // all groups share a same Engine instance
+}
+
+// Constructor of engine
 func New() *Engine {
-	return &Engine{router: newRouter()}
-}
-
-// Handle GET request for the path pattern
-// @params pattern[string] - path
-// @params handler[HandlerFunc] - call back function
-//
-// @return
-func (engine *Engine) GET(pattern string, handler HandlerFunc) {
-	engine.addRoute("GET", pattern, handler)
-}
-
-// Handle POST request for the path pattern
-// @params pattern[string] - path
-// @params handler[HandlerFunc] - call back function
-//
-// @return
-func (engine *Engine) POST(pattern string, handler HandlerFunc) {
-	engine.addRoute("POST", pattern, handler)
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
 // Run the engine in the address
@@ -46,13 +41,72 @@ func (engine *Engine) Run(address string) (err error) {
 	return http.ListenAndServe(address, engine)
 }
 
+//################################################
+//########### Add route via Engine ###############
+//################################################
 
-//################################ 
-//######## Helper Methods ######## 
-//################################ 
+// Handle GET request for the path pattern via Engine
+// @params pattern[string] - path
+// @params handler[HandlerFunc] - call back function
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
 
-func (engine *Engine) addRoute (method string, pattern string, handler HandlerFunc) {
+// Handle POST request for the path pattern via Engine
+// @params pattern[string] - path
+// @params handler[HandlerFunc] - call back function
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
+}
+
+//###############################################
+//########### Add route via Group ###############
+//###############################################
+
+// Create a new group
+// @params prefix[string] - prefix path
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	// All groups share the same Engine instance
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix, // nesting
+		parent: group,                 // nesting
+		engine: engine,
+	}
+
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+// Handle POST request for the path pattern via Group
+// @params pattern[string] - path
+// @params handler[HandlerFunc] - call back function
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
+}
+
+// Handle POST request for the path pattern via Group
+// @params pattern[string] - path
+// @params handler[HandlerFunc] - call back function
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
+}
+
+//################################
+//######## Helper Methods ########
+//################################
+
+// Add route via engine
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	log.Printf("Register Route %4s - %s", method, pattern)
 	engine.router.addRoute(method, pattern, handler)
+}
+
+// Add route via group
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Register Route %4s - %s", method, pattern)
+	group.engine.router.addRoute(method, pattern, handler)
 }
 
 // To pass an engine to http.ListenAndServe
@@ -61,5 +115,3 @@ func (engine *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	c := newContext(w, r)
 	engine.router.handle(c)
 }
-
-
